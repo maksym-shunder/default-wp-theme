1ab320a592a8de1505c231b3ec298e7457764fa8
maksymDigiway <maksym@digiway.dev>
2025-11-13 16:15:52 +0200

    test auto deploy

--- template-parts/components/product-card.php
+++ template-parts/components/product-card.php
@@ -1,33 +1,42 @@
 <?php
-if($product_id):
+if ($product_id):
 	$product = wc_get_product($product_id);
-	if(!empty($product)):
+	if (!empty($product)):
 		$image_id = $product->get_image_id();
 		$image_url = wp_get_attachment_url($image_id);
 		$product_name = $product->get_name();
 		$product_permalink = $product->get_permalink();
 		$product_price = $product->get_price_html();
 		$tags = get_the_terms($product_id, 'product_tag');
-?>
-	<div class="product_box">
-		<a href="<?=$product_permalink;?>" class="img_box">
-			<img src="<?=$image_url?>" alt="<?=$product_name;?>" class="img_absolute">
+		?>
+		<div class="product_box 111">
+			<a
+				href="<?= $product_permalink; ?>"
+				class="img_box"
+			>
+				<img
+					src="<?= $image_url ?>"
+					alt="<?= $product_name; ?>"
+					class="img_absolute"
+				>
 
-			<?php if ($tags && !is_wp_error($tags)): ?>
-				<ul class="tags_box">
-					<?php foreach ($tags as $tag): ?>
-						<li class="tag bg-green-50"><?=esc_html($tag->name)?></li>
-					<?php endforeach; ?>
-				</ul>
-			<?php endif;?>
-		</a>
+				<?php if ($tags && !is_wp_error($tags)): ?>
+					<ul class="tags_box">
+						<?php foreach ($tags as $tag): ?>
+							<li class="tag bg-green-50"><?= esc_html($tag->name) ?></li>
+						<?php endforeach; ?>
+					</ul>
+				<?php endif; ?>
+			</a>
 
-		<div class="text_box">
-			<h3 class="text-green-700"><a href="<?=$product_permalink;?>"><?=$product_name;?></a></h3>
+			<div class="text_box">
+				<h3 class="text-green-700">
+					<a href="<?= $product_permalink; ?>"><?= $product_name; ?></a>
+				</h3>
 
-			<p><?=$product_price;?></p>
+				<p><?= $product_price; ?></p>
+			</div>
 		</div>
-	</div>
-<?php
+	<?php
 	endif;
 endif; ?>

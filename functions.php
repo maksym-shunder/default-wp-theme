209df9b2752ae0565f7f83e78154a9d11d69808d
maksymDigiway <maksym@digiway.dev>
2025-12-02 11:40:28 +0200

    rename utils to helpers

--- functions.php
+++ functions.php
@@ -8,6 +8,6 @@ require_once __DIR__ . '/inc/enqueue-scripts.php';
 
 require_once __DIR__ . '/inc/maintenance-page.php';
 
-require_once __DIR__ . '/inc/utils.php';
+require_once __DIR__ . '/inc/helpers.php';
 
 require_once __DIR__ . '/inc/woocommerce.php';
